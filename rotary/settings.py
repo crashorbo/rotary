98d7c2ec6d537bbb90e8dd01fb32baa03e497c0f
Alexander Calani <alexander.calani.apaza@gmail.com>
2018-01-25 19:30:43 -0400

    Agregando la url a hosts validos

--- rotary/settings.py
+++ rotary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h!r5(6_dkny)$z%28uwqj3sa$yqjs#rf&b3ggxrdgsj5x=*^hb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["crashorbo.pythonanywhere.com", "localhost"]
+ALLOWED_HOSTS = ["www.rotarysanmiguel.org", "localhost"]
 
 
 # Application definition
