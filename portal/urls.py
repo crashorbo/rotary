70b8178c131ecd19ea1482c2ecc9041f2f9e9e4e
Alexander Calani <alexander.calani.apaza@gmail.com>
2018-03-09 19:31:00 -0400

    se agrego funcionaldiad editar

--- portal/urls.py
+++ portal/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from .views import index, inicio, ParticipanteList, ParticipanteCreate
+from .views import index, inicio, ParticipanteList, ParticipanteCreate, ParticipanteUpdate
 
 urlpatterns = [
     url(r'^$', index, name='home'),
     url(r'^portal/$', inicio, name='inicio'),
     url(r'^portal/participante/$', ParticipanteList.as_view(), name='participante'),
-    url(r'^portal/participante/registro/$', ParticipanteCreate.as_view(), name='participante_create')
+    url(r'^portal/participante/registro/$', ParticipanteCreate.as_view(), name='participante_create'),
+    url(r'^portal/participante/editar/(?P<pk>\d+)/$', ParticipanteUpdate.as_view(), name='participante_update')
 ]
--- portal/views.py
+++ portal/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse_lazy
 from django.utils.decorators import method_decorator
 
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, UpdateView
 from .models import Participante
 from .forms import ParticipanteForm
 
@@ -28,6 +28,7 @@ class ParticipanteList(ListView):
     def dispatch(self, *args, **kwargs):
         return super(ParticipanteList, self).dispatch(*args, **kwargs)
 
+
 class ParticipanteCreate(CreateView):
     model = Participante
     form_class = ParticipanteForm
@@ -42,3 +43,17 @@ class ParticipanteCreate(CreateView):
     def dispatch(self, *args, **kwargs):
         return super(ParticipanteCreate, self).dispatch(*args, **kwargs)
 
+
+class ParticipanteUpdate(UpdateView):
+    model = Participante
+    form_class = ParticipanteForm
+    template_name = 'portal/participante_registro.html'
+    success_url = reverse_lazy('participante')
+
+    def form_valid(self, form):
+        form.save(self.request.user)
+        return super(ParticipanteUpdate, self).form_valid(form)
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(ParticipanteUpdate, self).dispatch(*args, **kwargs)
