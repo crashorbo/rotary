1fc0817f1f5df302541ff982961da14886348cac
Alexander Calani <alexander.calani.apaza@gmail.com>
2018-03-08 21:01:31 -0400

    cambios settings

--- rotary/serverset.py
+++ rotary/serverset.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'rotarysanmiguel$rotary',
         'USER': 'rotarysanmiguel',
-        'PASSWORD': 'alexbase',
+        'PASSWORD': 'alexrotar',
         'HOST': 'rotarysanmiguel.mysql.pythonanywhere-services.com',
     }
 }
